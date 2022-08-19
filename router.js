897ad02dd7734ae5b1f43bb5d732af4e473665d7
matthewvolny <matthew.volny@gmail.com>
2022-08-19 17:40:32 -0400

    added response body with change, and number of drinks remainig after vending drink

--- router.js
+++ router.js
@@ -36,7 +36,6 @@ router.put("/", async (req, res) => {
 //retrieve total coins
 router.get("/sum", async (req, res) => {
   console.log("in totalCoins");
-
   try {
     const sum = await database.any(`SELECT SUM(coins) FROM coins_deposited`);
     coinTotal = sum;
@@ -59,6 +58,8 @@ router.get("/inventory", async (req, res) => {
   }
 });
 
+//!change all these instances of 'userInfo'
+
 //retrieve specific inventory
 router.get("/inventory/:item", async (req, res) => {
   console.log("in get /inventory/:item");
@@ -76,26 +77,55 @@ router.get("/inventory/:item", async (req, res) => {
 //middleware to get number of coins before knowing if you can vend (can be used in various places)
 router.use("/inventory/:item", async (req, res, next) => {
   console.log("in middleware - get change");
-  //!need following line for when there is no money deposited
-  // if (moneyDeposited) {
-  if (true) {
-    try {
-      const sum = await database.any(`SELECT SUM(coins) FROM coins_deposited`);
-      coinTotal = sum[0].sum;
-      console.log(coinTotal);
+  try {
+    const sum = await database.any(`SELECT SUM(coins) FROM coins_deposited`);
+    coinTotal = sum[0].sum;
+    console.log(coinTotal);
+    if (coinTotal < 2) {
+      res
+        .status(403)
+        .json({
+          message:
+            "Insufficient funds.  Add more $ or click coin return button",
+        })
+        .end();
+    } else {
       res.locals.difference = coinTotal - 2;
-    } catch (error) {
-      console.log("sorry,stuck in middleware - get change");
     }
+  } catch (error) {
+    console.log("sorry,stuck in middleware - get change");
   }
   next();
 });
 
-//vend item and return excess coins
-router.put("/inventory/:item", async (req, res) => {
-  if (res.locals.difference) {
-    console.log("difference from middleware", res.locals.difference);
+//middleware to get remaining drinks
+router.use("/inventory/:item", async (req, res, next) => {
+  console.log("in middleware - get remaining drinks");
+
+  try {
+    const drink = await database.any(
+      `SELECT * FROM inventory WHERE drink_name = '${req.params.item}'`
+    );
+    console.log("drink quantity:", drink[0].quantity);
+    const drinkQuantity = drink[0].quantity;
+    if (drinkQuantity < 2) {
+      res
+        .status(404)
+        .json({
+          message: "Drink is out of stock.  Make another selection",
+        })
+        .end();
+    } else {
+      res.locals.remainingItem = drinkQuantity - 1;
+    }
+  } catch (error) {
+    console.log("sorry,stuck in middleware - get change");
   }
+  next();
+});
+
+//vend item, return excess coins, report remaining drinks
+router.put("/inventory/:item", async (req, res) => {
   console.log("in vend put");
   console.log(req.params.item);
   const drink = req.params.item;
@@ -104,13 +134,19 @@ router.put("/inventory/:item", async (req, res) => {
     let queryString =
       "INSERT INTO vended (drink_name, quantity) VALUES ($1, $2)";
     await database.none(queryString, [drink, 1]);
+
+    //!update suppply of coins, and drinks
+
     //!want to return header here that says
-    //! believe i need middleware for this
     //1) X of coins returned, delete request (delete request, remove all coins)
     //2) current inventory of the chose drink remaining (get request, remaining drink of the vended type)
-    res.json({ message: `1 ${drink} vended` });
+    res.json({
+      message: `1 ${drink} vended`,
+      message2: `change is ${res.locals.difference} quarters `,
+      message3: `${res.locals.remainingItem} ${drink}s remaining`,
+    });
   } catch (error) {
-    console.log("sorry, cannot accept coin");
+    console.log("sorry, system error");
   }
 });
 
